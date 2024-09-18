91d66105be7e70a3cb7f6d6524e480dfc3eeb0fc
Jur1nforest <134168959+Jur1nforest@users.noreply.github.com>
2024-09-18 16:38:10 -0400

    Add files via upload

---
+++ sketch_27rgbledstrip/sketch_27rgbledstrip.ino
@@ -0,0 +1,30 @@
+#include <Adafruit_NeoPixel.h> // Include the Adafruit NeoPixel library
+
+#define LED_PIN 14 // NeoPixel LED strip
+#define NUM_LEDS 8 // Number of LEDs
+
+// Create an instance of the Adafruit_NeoPixel class
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LEDS, LED_PIN, NEO_GRB + NEO_KHZ800); 
+
+void setup() {
+  strip.begin(); // Initialize the NeoPixel strip
+  strip.show(); // Set initial color to black
+}
+
+void loop() {
+    randomSeed(0);
+
+  // Turn on LEDs one by one
+  for (int i = NUM_LEDS; i >= 0 ; i--) {
+    strip.setPixelColor(i, random(0,255), random(0,255), random(0,255)); // Set the color of the i-th LED to red
+    strip.show(); // Update the LED strip with the new colors
+    delay(100); // Wait for 100 milliseconds
+  }
+  
+  // Turn off LEDs one by one
+  for (int i = NUM_LEDS; i >= 0; i--) {
+    strip.setPixelColor(i, 0, 0, 0); // Set the color of the i-th LED to black (turn it off)
+    strip.show(); // Update the LED strip with the new colors
+    delay(100); // Wait for 100 milliseconds
+  }
+}
